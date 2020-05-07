01fbcaf7dd620e2235f3598351b94860297b7ccd
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-05-06 19:31:50 -0500

    Add Animal class

---
+++ src/classes/Animal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author jpcv2
+ */
+public class Animal {
+
+    private String name;
+    private int weight;
+
+        public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}
