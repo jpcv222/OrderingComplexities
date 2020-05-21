4806a8435c799fc2b85a5c563fbfef73b5b2fc57
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2020-05-21 02:05:42 -0500

    Update linear complexity

--- src/managers/manager_index.java
+++ src/managers/manager_index.java
@@ -199,13 +199,55 @@ public class manager_index {
         int n = parts.length;
         sortParts(parts, 0, n - 1);
     }
+    
+    public void radixsort() 
+    { 
+        // Find the maximum number to know number of digits 
+        int m = getMaxOverallGreatnessParts(this.parts); 
+  
+        // Do counting sort for every digit. Note that instead 
+        // of passing digit number, exp is passed. exp is 10^i 
+        // where i is current digit number 
+        for (int exp = 1; m/exp > 0; exp *= 10) 
+            countSort(this.parts, this.m - 1, exp); 
+    } 
+    
+    public void countSort(Part arr[], int n, int exp) 
+    { 
+        Part output[] = new Part[n]; // output array 
+        int i; 
+        int count[] = new int[10]; 
+        Arrays.fill(count,0); 
+  
+        // Store count of occurrences in count[] 
+        for (i = 0; i < n; i++) 
+            count[ (arr[i].getOverall_greatness()/exp)%10 ]++; 
+  
+        // Change count[i] so that count[i] now contains 
+        // actual position of this digit in output[] 
+        for (i = 1; i < 10; i++) 
+            count[i] += count[i - 1]; 
+  
+        // Build the output array 
+        for (i = n - 1; i >= 0; i--) 
+        { 
+            output[count[ (arr[i].getOverall_greatness()/exp)%10 ] - 1] = arr[i]; 
+            count[ (arr[i].getOverall_greatness()/exp)%10 ]--; 
+        } 
+  
+        // Copy the output array to arr[], so that arr[] now 
+        // contains sorted numbers according to curent digit 
+        for (i = 0; i < n; i++) 
+            arr[i] = output[i]; 
+    } 
 
     public void linearComplexity() {
         int max;
         //Ordenar m - 1 partes
         System.out.println("entre O(n)");
             max = getMaxOverallGreatnessParts(this.parts); //O(n)
-            this.parts = countingSortParts(this.parts, max); //O(n)
+            //this.parts = countingSortParts(this.parts, max); //O(n)
+            radixsort();
         
     }
 
